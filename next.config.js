d61bfc43612599e9dd08f5bd6aa40cc59be811dc
yyds <thesoleyou@gmail.com>
2026-04-18 08:57:09 +0800

    upload all components and lib files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'zh', 'zh-TW'],
-    defaultLocale: 'zh',
-  },
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
