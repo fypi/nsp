c7511907cf31f6988c1658bdc9e2dcfc5737cd77
yyds <thesoleyou@gmail.com>
2026-04-18 18:20:53 +0800

    i18n fully fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true
-  }
-}
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
