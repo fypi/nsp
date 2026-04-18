bc8100c0f4776013339c8ed9ef991fcee275e78f
yyds <thesoleyou@gmail.com>
2026-04-18 18:26:17 +0800

    i18n fully fixed

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
+  experimental: {
+    turbo: false,   // ⭐ 关闭 Turbopack
+  },
 };
 
 module.exports = nextConfig;
