c7a1bfa28508e61fda21eb67dcefa8b40edcde12
yyds <thesoleyou@gmail.com>
2026-04-23 22:50:19 +0800

    add root redirect to zh

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "40px", fontSize: "24px" }}>
+      <h1>欢迎来到 NSP</h1>
+      <p>你的 Next.js 网站已经成功部署。</p>
+    </main>
+  );
+}
