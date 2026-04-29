d16f53f502a67c7414fb162a9f7ff113d8722d46
yyds <thesoleyou@gmail.com>
2026-04-29 19:36:23 +0800

    i18n fully fixed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,14 +2,9 @@
 
 export default function Layout({ children }) {
   return (
-    <html lang="zh-CN">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body style={{ margin: 0, padding: 0 }}>
-        <Navbar />
-        <main style={{ paddingTop: '60px' }}>{children}</main>
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+    </>
   );
 }
